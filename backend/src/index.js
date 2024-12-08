369e8e2f7e2993153dc6f78d693eab12cac1515a
ajithkumar-v-1 <221afa03ajithkumar@gmail.com>
2024-12-08 18:47:05 +0530

    Project completedt

---
+++ backend/src/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoute from "./routes/message.route.js";
+import cors from "cors";
+import { app, httpServer } from "./lib/socket.js";
+
+import path from "path";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
+httpServer.listen(PORT, () => {
+  console.log("Server is running on PORT", PORT);
+  connectDB();
+});
+
---
+++ backend/src/lib/Socket.js
@@ -0,0 +1,40 @@
+import { Server as SocketIOServer } from "socket.io"; // Rename to avoid conflict
+import http from "http";
+import express from "express";
+
+const app = express();
+const httpServer = http.createServer(app); // Use a distinct name
+
+const io = new SocketIOServer(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// used to store online users
+const userSocketMap = {}; 
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+})
+
+
+export { httpServer, io, app };
