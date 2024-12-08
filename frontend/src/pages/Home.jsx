d24695e1db4cf1cdf95231d3c6c787a51cfc1dae
ajithkumar-v-1 <221afa03ajithkumar@gmail.com>
2024-12-08 21:21:48 +0530

    project completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import { useChatStore } from "../store/useChatStore";
 
 import Sidebar from "../components/Sidebar";
-import NoChatSelected from "../components/NoChatSelected";
+import NoChatSelected from "../components/NoChatselected";
 import ChatContainer from "../components/ChatContainer";
 
 const Home = () => {
